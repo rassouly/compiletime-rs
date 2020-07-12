8743a6d94dcf99f8f3a4cc7aeebd4c3f715aa0a8
aspen <luxx4x@protonmail.com>
2020-07-11 20:05:20 -0400

    Revert "Add"
    This reverts commit 518354a207630d1ade700bcf19148cfc4151cfe2.

--- src/lib.rs
+++ src/lib.rs
@@ -9,23 +9,6 @@ lazy_static! {
     static ref NOW: SystemTime = SystemTime::now();
 }
 
-/// Returns either the "SOURCE_DATE_EPOCH" if it is set, or if it is not available, the number of seconds since midnight 1 January 1970.
-#[proc_macro]
-pub fn source_data_epoch(input: TokenStream) -> TokenStream {
-    std::mem::drop(input);
-
-    let lit = if let Ok(sde) = std::env::var("SOURCE_DATE_EPOCH") {
-        syn::LitInt::new(&sde, Span::call_site())
-    } else {
-        let since_epoch = NOW
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards")
-            .as_secs();
-        syn::LitInt::new(&since_epoch.to_string(), Span::call_site())
-    };
-    return quote! { (#lit as u64) }.into();
-}
-
 /// The number of milliseconds since midnight 1 January 1970.
 #[proc_macro]
 pub fn milliseconds(input: TokenStream) -> TokenStream {
